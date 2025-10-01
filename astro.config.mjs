52ac28d924bc148e780278d4f0b52e38402ed164
Javier Castro <javiercastro.ik@gmail.com>
2025-10-01 17:49:09 -0500

    avance qa

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   compressHTML: false,
   build: {
     assets: '_assets',
-    // assetsPrefix: 'https://www.claro.com.pe/assets/havas/prepago'
+    assetsPrefix: 'https://www.claro.com.pe/assets/havas/mi-claro-app'
   },
   vite: {
     plugins: [tailwindcss()]
